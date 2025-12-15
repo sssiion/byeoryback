beabbe9f628d64527cd2c10d4509f1dfba4d6b6c
Im-uisoon <dla5420@naver.com>
2025-12-15 19:43:39 +0900

    프로젝트 세팅

---
+++ src/main/java/com/project/byeoryback/ByeorybackApplication.java
@@ -0,0 +1,13 @@
+package com.project.byeoryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeorybackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByeorybackApplication.class, args);
+    }
+
+}
