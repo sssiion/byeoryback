d2a8ac7636ff0920c0e085eb40311c595c90e77a
sssiion <rta2752775@gmail.com>
2026-01-09 14:03:02 +0900

    스티커 위젯
    스티커처럼 활용함

--- src/main/java/com/project/byeoryback/domain/post/entity/FloatingItem.java
+++ src/main/java/com/project/byeoryback/domain/post/entity/FloatingItem.java
@@ -1,5 +1,6 @@
 package com.project.byeoryback.domain.post.entity;
 
+import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -23,4 +24,7 @@ public class FloatingItem {
     private String url; // 이미지 URL
     private String text; // 텍스트 내용 (FloatingText용)
     private Map<String, Object> styles; // 스타일
+
+    @Column(columnDefinition = "json") // JPA 환경에 따라 어노테이션 필요할 수 있음
+    private Map<String, Object> widgetProps;
 }
